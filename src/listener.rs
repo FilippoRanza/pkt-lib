b005779d4002aa853c3ee950793361a6ac703bc0
Filippo Ranza <filipporanza@gmail.com>
2021-08-27 18:28:29 +0200

    Use tcp instead of udp

--- src/listener.rs
+++ src/listener.rs
@@ -1,8 +1,8 @@
 use std::net::{IpAddr, SocketAddr};
 use std::sync::mpsc;
 
-use tokio::net::UdpSocket;
-
+use tokio::io::AsyncReadExt;
+use tokio::net::{TcpListener, TcpStream};
 
 type Handler = tokio::task::JoinHandle<std::io::Result<()>>;
 type Receiver<T> = mpsc::Receiver<RecvInfo<T>>;
@@ -37,7 +37,7 @@ pub struct RecvInfo<T> {
     pub addr: SocketAddr,
 }
 
-pub fn create_udp_listener<F, T, K>(ip: IpAddr, port: u16, f: &'static F) -> ListenerController<K>
+pub fn create_tcp_listener<F, T, K>(ip: IpAddr, port: u16, f: &'static F) -> ListenerController<K>
 where
     F: Fn(&T) -> K + Sync,
     T: Default + AsMut<[u8]> + Send,
@@ -46,12 +46,12 @@ where
     let (info_trans, info_recv) = mpsc::channel();
     let (control_trans, control_recv) = mpsc::channel();
     let addr = SocketAddr::new(ip, port);
-    let handle = start_udp_listener(addr, f, info_trans, control_recv);
+    let handle = start_tcp_listener(addr, f, info_trans, control_recv);
 
     ListenerController::new(control_trans, info_recv, handle)
 }
 
-fn start_udp_listener<F, T, K>(
+fn start_tcp_listener<F, T, K>(
     addr: SocketAddr,
     f: &'static F,
     send: mpsc::Sender<RecvInfo<K>>,
@@ -59,23 +59,46 @@ fn start_udp_listener<F, T, K>(
 ) -> Handler
 where
     F: Fn(&T) -> K + Sync,
-    T:  Default + AsMut<[u8]> + Send,
+    T: Default + AsMut<[u8]> + Send,
     K: Send + Sync + 'static,
 {
-    tokio::spawn( async move {
-        let sock = UdpSocket::bind(addr).await?;
-        let mut buff = T::default();
+    tokio::spawn(async move {
+        let listener = TcpListener::bind(addr).await?;
         while should_continue(&recv) {
-            let (amt, addr) = sock.recv_from(buff.as_mut()).await?;
-            if amt > 0 {
-                let data = f(&buff);
-                let info = RecvInfo { data, addr };
-                send.send(info).unwrap();
-            }
+            let accept = listener.accept().await?;
+            let clone_send = send.clone();
+            tokio::spawn(async move {
+                let (sock, addr) = accept;
+                handle_connetion(sock, addr, f, clone_send).await;
+            });
         }
 
         Ok(())
     })
+}
+
+async fn handle_connetion<F, T, K>(
+    mut sock: TcpStream,
+    addr: SocketAddr,
+    f: &'static F,
+    send: mpsc::Sender<RecvInfo<K>>,
+) where
+    F: Fn(&T) -> K + Sync,
+    T: Default + AsMut<[u8]> + Send,
+    K: Send + Sync + 'static,
+{
+    let mut buff = T::default();
+
+    let res = sock.read(buff.as_mut()).await;
+    match res {
+        Ok(n) if n > 0 => {
+            let data = f(&buff);
+            let info = RecvInfo { data, addr };
+            send.send(info).unwrap();
+        }
+        Ok(_) => {}
+        Err(err) => println!("{:?}", err),
+    };
 }
 
 fn should_continue(recv: &mpsc::Receiver<()>) -> bool {
